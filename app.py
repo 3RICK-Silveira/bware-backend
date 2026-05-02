e3e94d3ec089d31841e778f9498b8f883afd4339
3RICK-Silveira <fnxerick425@gmail.com>
2026-05-02 16:05:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -329,5 +329,7 @@ def listar_pedidos():
 # ==============================
 # 🚀 RUN
 # ==============================
+    import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
